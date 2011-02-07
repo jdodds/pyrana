ef7340e3a51ae4037e266e5dd3b4fb16ee13f93a
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-02-07 01:05:04 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Pyrana",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     include_package_data=True,
     exclude_package_data={'': ['.gitignore']},
